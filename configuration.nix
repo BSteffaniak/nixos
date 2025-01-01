e32d9c3454518100c68c0d18e79d43d046698f0f
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-01-01 07:30:00 -0500

    Use seconary hard drive for docker data-root

--- configuration.nix
+++ configuration.nix
@@ -355,7 +355,12 @@
     magicOrExtension = ''\x7fELF....AI\x02'';
   };
 
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    daemon.settings = {
+      data-root = "/hdd/docker";
+    };
+  };
 
   # List services that you want to enable:
 
