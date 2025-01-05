a621ac5e75e34ff6889c3681ccb8a9e564a205bb
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-01-05 16:07:12 -0500

    Add bitwarden

--- home.nix
+++ home.nix
@@ -20,6 +20,7 @@
   home.packages = with pkgs; [
     prismlauncher
     ungoogled-chromium
+    bitwarden-desktop
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
