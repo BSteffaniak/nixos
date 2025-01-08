2ce98840e899775a9a0bc405075ba2a4dedc2303
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-01-08 07:30:00 -0500

    Fix pipewire

--- configuration.nix
+++ configuration.nix
@@ -196,13 +196,15 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  # hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    # pulse.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     # jack.enable = true;
 
