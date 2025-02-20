b0d16416cdcd4ea16ec99918ed43fdc64dc80ec2
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-02-20 07:30:00 -0500

    Add natscli

--- home.nix
+++ home.nix
@@ -30,6 +30,7 @@
     gh-dash
     android-studio
     fd
+    natscli
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
