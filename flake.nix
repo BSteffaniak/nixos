8aa428ea5f155d25fe5a00d67b8f570de61f8fa6
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-09-22 07:30:00 -0400

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    wezterm.url = "github:wez/wezterm?dir=nix";
-    ra-multiplex.url = "github:pr2502/ra-multiplex";
+    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     swww.url = "github:LGFae/swww";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -32,6 +31,21 @@
                 unstable = import nixpkgs-unstable {
                   inherit (prev) system;
                   config.allowUnfree = true;
+                };
+
+                # Add ra-multiplex from GitHub
+                ra-multiplex-latest = final.rustPlatform.buildRustPackage {
+                  pname = "ra-multiplex";
+                  version = "unstable-2024-08-30";
+
+                  src = final.fetchFromGitHub {
+                    owner = "pr2502";
+                    repo = "ra-multiplex";
+                    rev = "master";
+                    sha256 = "12x3rm9swnx21wllpbfwg5q4jvjr5ha6jn13dg2gjsbp0swbzqly";
+                  };
+
+                  cargoHash = "sha256-PnZh6wBMul3D4lsUQdn7arF2Qng2vdqtZHpPOtN59eU=";
                 };
               })
             ];
