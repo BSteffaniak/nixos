fda5983800374d95723d15aab87aa03ad1f7727c
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-10-22 22:35:28 -0400

    Add opencode official

--- home.nix
+++ home.nix
@@ -52,6 +52,7 @@ in
     mediainfo
     flac
     unstable.bun
+    unstable.opencode
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
