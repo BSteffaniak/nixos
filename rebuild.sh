cfc3c7372c4c6c87866f7d9108e804f7a8d44c58
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-10-31 07:30:00 -0400

    Support --boot argument

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 sudo cp ./* /etc/nixos/
-sudo nixos-rebuild switch $@
+
+# Build a new argument array without --boot
+filtered_args=()
+boot=false
+for arg in "$@"; do
+    if [[ "$arg" != "--boot" ]] || [[ "$boot" == "true" ]]; then
+        filtered_args+=("$arg")
+    else
+        boot=true
+    fi
+done
+
+if [[ "$boot" == true ]]; then
+    sudo nixos-rebuild boot "${filtered_args[@]}"
+else
+    sudo nixos-rebuild switch "$@"
+fi
