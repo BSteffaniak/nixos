2c97b0ed61cad019efa5e4e9fff0544e10a9f2d7
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-11-12 21:21:09 -0500

    Add base home config to mac-studio & macbook air

--- hosts/mac-studio/home.nix
+++ hosts/mac-studio/home.nix
@@ -18,6 +18,25 @@
   };
 
   myConfig = {
+    # CLI tools configs
+    cli-tools.bottom.enable = true;
+    cli-tools.htop.enable = true;
+    cli-tools.terminals.enable = true;
+    cli-tools.tmux.enable = true;
+
+    # Development tool configs
+    development.lazygit.enable = true;
+    development.act.enable = true;
+    development.opencode.enable = true;
+    development.ra-multiplex.enable = true;
+
+    # DevOps tool configs
+    devops.github = {
+      enable = true;
+      username = "BSteffaniak";
+      gitProtocol = "ssh";
+    };
+
     shell.ssh = {
       matchBlocks = {
         "github.com" = {
